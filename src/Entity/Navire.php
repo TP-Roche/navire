ee4bc357279f92795e118cf12c5b7f3cea377754
yann.montenot <yann.montenot@CP-SIO-W11-PG06.bts.sio>
2023-12-13 17:00:01 +0100

    page 10

---
+++ src/Entity/Navire.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NavireRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: NavireRepository::class)]
+class Navire {
+
+    #[Assert\Unique(fields: ['IMO', 'MMSI', 'indicatifAppel'])]
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(name: 'id')]
+    private ?int $id = null;
+
+    #[ORM\Column(name: 'IMO', length: 7)]
+    #[Assert\Regex('[1-9][0-9]{6}', message: 'le numéro IMO doit être unique et composé de 7 chiffres sans commencer par 0')]
+    private ?string $IMO = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 9)]
+    private ?string $MMSI = null;
+
+    #[ORM\Column(length: 10)]
+    private ?string $indicatifAppel = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $eta = null;
+
+    #[ORM\Column]
+    private ?int $longueur = null;
+
+    #[ORM\Column]
+    private ?int $largeur = null;
+
+    #[ORM\Column]
+    private ?int $tirantdeau = null;
+
+    #[ORM\ManyToOne(inversedBy: 'navires')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?AisShipType $type = null;
+
+    public function getId(): ?int {
+        return $this->id;
+    }
+
+    public function getIMO(): ?string {
+        return $this->IMO;
+    }
+
+    public function setIMO(string $IMO): static {
+        $this->IMO = $IMO;
+
+        return $this;
+    }
+
+    public function getNom(): ?string {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getMMSI(): ?string {
+        return $this->MMSI;
+    }
+
+    public function setMMSI(string $MMSI): static {
+        $this->MMSI = $MMSI;
+
+        return $this;
+    }
+
+    public function getIndicatifAppel(): ?string {
+        return $this->indicatifAppel;
+    }
+
+    public function setIndicatifAppel(string $indicatifAppel): static {
+        $this->indicatifAppel = $indicatifAppel;
+
+        return $this;
+    }
+
+    public function getEta(): ?\DateTimeInterface {
+        return $this->eta;
+    }
+
+    public function setEta(?\DateTimeInterface $eta): static {
+        $this->eta = $eta;
+
+        return $this;
+    }
+
+    public function getLongueur(): ?int {
+        return $this->longueur;
+    }
+
+    public function setLongueur(int $longueur): static {
+        $this->longueur = $longueur;
+
+        return $this;
+    }
+
+    public function getLargeur(): ?int {
+        return $this->largeur;
+    }
+
+    public function setLargeur(int $largeur): static {
+        $this->largeur = $largeur;
+
+        return $this;
+    }
+
+    public function getTirantdeau(): ?int {
+        return $this->tirantdeau;
+    }
+
+    public function setTirantdeau(int $tirantdeau): static {
+        $this->tirantdeau = $tirantdeau;
+
+        return $this;
+    }
+
+    public function getType(): ?AisShipType
+    {
+        return $this->type;
+    }
+
+    public function setType(?AisShipType $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+}
